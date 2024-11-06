7093861370842c22c68799f04f05f8ed9e051742
Sandom <138884502+Sandomhet@users.noreply.github.com>
2024-11-05 20:36:01 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Box, Container, Drawer } from "@mui/material";
+import { blue, deepPurple } from "@mui/material/colors";
+import TopNavigation from "./components/TopNavigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      <Box
+        sx={{
+          flex: 1,
+          width: 250,
+          // height: "100vh",
+          // margin: 1,
+          border: "5px solid red",
+          padding: "20px",
+          bgcolor: blue[500],
+        }}
+      >
+        Profile
+      </Box>
+      <Box
+        sx={{ flex: 4, border: "5px solid yellow", bgcolor: deepPurple[500] }}
+      >
+        <TopNavigation />
+        {/* <AppBar sx={{ position: "sticky" }}>Blogs</AppBar> */}
+        <Container>Latest News</Container>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/components/TopNavigation.js
@@ -0,0 +1,7 @@
+import { AppBar } from "@mui/material";
+
+function TopNavigation() {
+  return <AppBar sx={{ position: "sticky" }}>Blogs</AppBar>;
+}
+
+export default TopNavigation;
