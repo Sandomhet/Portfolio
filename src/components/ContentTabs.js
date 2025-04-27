0f68242edc7b1b2fddb0e364638230ac6c12cb9d
Sandom <sandomhet@gmail.com>
2025-04-26 23:12:00 -0700

    cleaned up packages

--- src/components/ContentTabs.js
+++ src/components/ContentTabs.js
@@ -1,16 +1,23 @@
-import {Box, List, Typography} from "@mui/material";
+import {Box, Button, Container, List, ToggleButton, ToggleButtonGroup, Typography} from "@mui/material";
 import PreviewWidget from "./PreviewWidget";
 import {useParams} from "react-router-dom";
 import files from "../assets/mdStorage.json";
+import {useState} from "react";
 
 export default function ContentTabs() {
+  
   const {type} = useParams();
-  let filtered = files.filter((file) => {
-    if (type === undefined) {
-      return true;
-    }
-    return file.type === type;
-  });
+  const filteredByType = type ? files.filter(file => file.type === type) : files;
+  const categories = type === "blogs"
+      ? ["All", ...new Set(
+          filteredByType
+              .map(file => file.category)
+              .filter(category => category.trim() !== "")
+      )]
+      : [];
+  
+  const [activeCategory, setActiveCategory] = useState("All");
+  const filtered = activeCategory === "All" ? filteredByType : filteredByType.filter(file => file.category === activeCategory);
   
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -21,6 +28,21 @@ export default function ContentTabs() {
         <Typography variant="h2" sx={{fontWeight: "Bold"}}>
           {type ? capitalizeFirstLetter(type) : "All"}
         </Typography>
+        {categories.length > 0 && (
+            <Box sx={{display: "flex", justifyContent: "space-between"}}>
+              {categories.map(category => (
+                  <Button
+                      key={category}
+                      color={"black"}
+                      // variant="outlined"
+                      variant={activeCategory === category ? "contained" : "outlined"}
+                      onClick={() => setActiveCategory(category)}
+                  >
+                    {category}
+                  </Button>
+              ))}
+            </Box>
+        )}
         <List>
           {filtered.map((file) => (
               <List key={file.name}>
